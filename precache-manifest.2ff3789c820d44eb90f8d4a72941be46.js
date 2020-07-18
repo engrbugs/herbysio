3c7764ce7e6123f699eb11e0cb90aaae336bbd4a
engrbugs <engrbugs@gmail.com>
2020-07-18 11:50:38 -0700

    Updates

---
+++ precache-manifest.2ff3789c820d44eb90f8d4a72941be46.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bb22cfb5611666e7cf9bc0a9085bd81",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b224b5d2db5b5ff1595e",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2eed21bb2605db97d24a",
+    "url": "/static/js/2.619e6845.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.619e6845.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b224b5d2db5b5ff1595e",
+    "url": "/static/js/main.862c83f9.chunk.js"
+  },
+  {
+    "revision": "528ec9d728a6962b2ee7",
+    "url": "/static/js/runtime-main.e12977cf.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
