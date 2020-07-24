2baf1bc7270b24b972287f2d53463504844f4965
engrbugs <engrbugs@gmail.com>
2020-07-24 09:41:07 -0700

    2020-07-24-0941

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import EmailMe from "./components/email-icon.js";
 import FooterIcon from "./components/footer-icon";
 import ScrollDownArrow from "./components/scroll-down-arrow.js"
 import DiagonalSection from "./components/diagonal-section.js"
+import "./components/tool-tip.css"
 
 export const WEBSITE_EMAIL = "engrherbysio@gmail.com";
 
@@ -13,9 +14,11 @@ function App() {
   return (
     <div className="App grid">
       <header className="container-fluid welcome-section p-0 m-0">
-        <div className="mail" title="email me">
-          <EmailMe />
+      
+        <div className="mail">
+        <p className="tooltip--left" data-tooltip="email me"><EmailMe /></p>
         </div>
+        
         <DiagonalSection />
         <div className="row text-align-center justify-content-center m-0">
           <div className="image-cropper">
@@ -43,14 +46,14 @@ function App() {
       </header>
       <section id="coming-soon" className="coming-soon-section">
         <div className="container-fluid">
-          <div className="row text-align-center justify-content-center underconstruction">
-            <p className="pt-5 my-0 pb-0 h-auto d-inline-block">#</p>
-            <p className="pt-5 my-0 pb-0 h-auto d-inline-block">underconstruction</p>
+          <div className="row text-align-center justify-content-center">
+            <p className="pt-5 my-0 pb-0 h-auto d-inline-block pound">#</p>
+            <p className="pt-5 my-0 pb-0 h-auto d-inline-block underconstruction">underconstruction</p>
           </div>
-          <div className="row text-align-center justify-content-center pt-0 mt-0">
+          <div className="row text-align-center justify-content-center pt-0 mt-2">
             <h1>Coming Soon</h1>
           </div>
-          <div className="row text-align-center justify-content-center">
+          <div className="row text-align-center justify-content-center mt-2 checkout">
             <p>Check out online portfolios:</p>
           </div>
         </div>
